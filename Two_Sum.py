0226268124eac8476193b42aca55f3838034d820
fanfanda <fanfanda@ict.ac.cn>
2019-01-31 12:08:51 +0800

    something interesting

---
+++ Two_Sum.py
@@ -0,0 +1,33 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        temp_dict = {}
+
+        for index, item in enumerate(nums):
+            try:
+                return [index, temp_dict[target - item]]
+            except KeyError:
+                temp_dict[item] = index
+
+        # temp_dict = {}
+
+        # for index, item in enumerate(nums):
+        #     temp_dict[item] = index
+        
+        # for index, item in enumerate(nums):
+        #     try:
+        #         if index != temp_dict[target - item]:
+        #             return [index, temp_dict[target - item]]
+        #     except KeyError:
+        #         continue
+
+nums = [2, 7, 11, 15]
+target = 9
+
+t = Solution()
+print(t.twoSum(nums, target))
