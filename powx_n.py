e6f73d2e63529e63f361428c96fdef5ccfa76be4
fanfanda <fanfanda@ict.ac.cn>
2019-03-25 10:25:04 +0800

    不用实现小于1的情况

--- powx_n.py
+++ powx_n.py
@@ -8,15 +8,6 @@ class Solution:
             return half * half
         else:
             return half * half * x
-# class Solution:
-#     def myPow(self, a, b):
-#         if b == 0: return 1
-#         if b < 0: return 1.0 / self.myPow(a, -b)
-#         half = self.myPow(a, b // 2)
-#         if b % 2 == 0:
-#             return half * half
-#         else:
-#             return half * half * a
 
 t = Solution()
 print(t.myPow(3, 0.1))
