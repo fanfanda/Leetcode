cefa833635a0b7124a7de165bf25bb47966184e7
fanfanda <fanfanda@ict.ac.cn>
2019-04-02 22:54:02 +0800

    nothing

--- quick_sort.py
+++ quick_sort.py
@@ -1,46 +1,17 @@
-
-def quicksort_helper(nums, r, q):
-    if r < q:
-        k = partition(nums, r, q)
-        quicksort_helper(nums, r, k)
-        quicksort_helper(nums, k + 1, q)
-def partition(nums, r, q):
-    pivot = nums[r]
-    while r < q:
-        while r < q and nums[q] >= pivot:
-            q -= 1
-        if r < q:
-            nums[r] = nums[q]
-        while r < q and nums[r] < pivot:
-            r += 1
-        if r < q:
-            nums[q] = nums[r]
-
-    nums[r] = pivot
-    return r
-
-def quick_sort_standord(array,low,high):
-    ''' realize from book "data struct" of author 严蔚敏
-    '''
-    if low < high:
-        key_index = partion(array,low,high)
-        quick_sort_standord(array,low,key_index)
-        quick_sort_standord(array,key_index+1,high)
-
-def partion(array,low,high):
-    key = array[low]
-    while low < high:
-        while low < high and array[high] >= key:
-            high -= 1
-        if low < high:
-            array[low] = array[high]
-
-        while low < high and array[low] < key:
-            low += 1
-        if low < high:
-            array[high] = array[low]
-
-    array[low] = key
-    return low
+def quick_sort(A, left, right):
+    if left < right:
+        k = partition(A, left, right)
+        quick_sort(A, left, k)
+        quick_sort(A, k + 1, right)
+    
+def partition(A, left, right):
+    pivot = A[left]
+    while left < right:
+        while left < right and A[right] > pivot: right -= 1
+        if left < right: A[left] = A[right]
+        while left < right and A[left] < pivot: left += 1
+        if left < right: A[right] = A[left]
+    A[left] = pivot 
+    return left
 t = [2, 5, 7, 1, 0]
 # quick_sort_standord(t, 0, len(t) - 1)
